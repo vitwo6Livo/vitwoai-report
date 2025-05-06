b915a34f1f9bed3052633b896825a4358b7b4849
chayan-vitwo <chayan@vitwo.in>
2025-05-06 11:27:02 +0530

    Chayan Changes

--- lib/src/sales_Register/presentation/SRWidget/itemWiseList.dart
+++ lib/src/sales_Register/presentation/SRWidget/itemWiseList.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:lottie/lottie.dart';
 import 'package:vitwoai_report/golobal-Widget/loadingShimmer.dart';
 import 'package:vitwoai_report/golobal-Widget/shimmer_screen.dart';
 import 'package:vitwoai_report/src/sales_Register/data/salesRegisterFatchData.dart';
@@ -150,6 +151,8 @@ class _ItemWiseScreenState extends ConsumerState<ItemWiseScreen> {
   @override
   Widget build(BuildContext context) {
     final salesRegisterItemList = ref.watch(salesRegisterItemListStateProvider);
+    double screenHeight = MediaQuery.of(context).size.height;
+    double screenWidth = MediaQuery.of(context).size.width;
 
     return Scaffold(
       backgroundColor: AppColor.screenBgColor,
@@ -248,7 +251,14 @@ class _ItemWiseScreenState extends ConsumerState<ItemWiseScreen> {
             child: _isInitialLoading
                 ? screen_shimmer(120, 800)
                 : salesRegisterItemList.content.isEmpty
-                    ? Center(child: Text(HandText.noData))
+                    ? Center(
+                        child: LottieBuilder.asset(
+                          'assets/json/NoDataFound.json',
+                          fit: BoxFit.fill,
+                          height: screenHeight * 0.38,
+                          width: screenWidth * 0.68,
+                        ),
+                      )
                     : ListView.builder(
                         controller: _scrollController,
                         itemCount: _isLoadingMore
