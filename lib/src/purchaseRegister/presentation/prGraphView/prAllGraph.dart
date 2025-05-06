a4d059afb04e8399715a77d97ddae4e2752efddf
chayan-vitwo <chayan@vitwo.in>
2025-05-06 15:21:56 +0530

    Chnages

--- lib/src/purchaseRegister/presentation/prGraphView/prAllGraph.dart
+++ lib/src/purchaseRegister/presentation/prGraphView/prAllGraph.dart
@@ -12,9 +12,13 @@ class ProductGraphModel extends ConsumerStatefulWidget {
 }
 
 class _ProductGraphModelState extends ConsumerState<ProductGraphModel> {
+  bool showQty = true;
+  String _selectedGraph = 'qty';
+
   @override
   Widget build(BuildContext context) {
     final PRGraphAll = ref.watch(prGraphAllProvider);
+
     return Card(
       color: Colors.white,
       surfaceTintColor: Colors.white,
@@ -37,80 +41,98 @@ class _ProductGraphModelState extends ConsumerState<ProductGraphModel> {
               ],
             ),
             const SizedBox(height: 40),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Radio<String>(
+                  value: 'qty',
+                  groupValue: _selectedGraph,
+                  onChanged: (value) {
+                    setState(() {
+                      _selectedGraph = value!;
+                    });
+                  },
+                ),
+                const Text('Total Qty'),
+                Radio<String>(
+                  value: 'amount',
+                  groupValue: _selectedGraph,
+                  onChanged: (value) {
+                    setState(() {
+                      _selectedGraph = value!;
+                    });
+                  },
+                ),
+                const Text('Total Amount'),
+              ],
+            ),
+
             PRGraphAll.when(
               data: (data) {
                 return SfCartesianChart(
-                  plotAreaBackgroundColor: Colors.transparent,
-                  margin: const EdgeInsets.all(0),
-                  borderColor: Colors.transparent,
-                  borderWidth: 0,
-                  plotAreaBorderWidth: 0,
                   enableSideBySideSeriesPlacement: true,
+                  plotAreaBackgroundColor: Colors.transparent,
                   primaryXAxis: const CategoryAxis(
                     axisLine: AxisLine(width: 0.5),
                     majorGridLines: MajorGridLines(width: 0),
                     majorTickLines: MajorTickLines(width: 0),
-                    crossesAt: 0,
                   ),
-                  primaryYAxis: const NumericAxis(
-                    isVisible: true,
-                    // minimum: 0,
-                    // maximum: 2,
-                    // interval: 0.5,
-                  ),
+                  primaryYAxis: const NumericAxis(isVisible: true),
                   series: <CartesianSeries>[
-                    ColumnSeries<PrGraphAllContent, String>(
-                      borderRadius: const BorderRadius.only(
-                        topLeft: Radius.circular(20),
-                        topRight: Radius.circular(20),
+                    if (_selectedGraph == 'qty')
+                      ColumnSeries<PrGraphAllContent, String>(
+                        name: 'Total Qty',
+                        dataSource: data.data,
+                        color: Colors.blue,
+                        width: 0.4,
+                        spacing: 0.2,
+                        borderRadius: const BorderRadius.only(
+                          topLeft: Radius.circular(8),
+                          topRight: Radius.circular(8),
+                        ),
+                        xValueMapper: (PrGraphAllContent data, _) =>
+                            data.TotalQty == '0' ? null : data.Month,
+                        yValueMapper: (PrGraphAllContent data, _) =>
+                            data.TotalQty == '0' ? null : data.TotalQty,
+                        emptyPointSettings: EmptyPointSettings(
+                          mode: EmptyPointMode
+                              .drop, // or .drop to remove it entirely
+                          color: Colors.transparent, // optional
+                        ),
+                      )
+                    else
+                      ColumnSeries<PrGraphAllContent, String>(
+                        name: 'Total Amount',
+                        dataSource: data.data,
+                        color: Colors.green,
+                        // emptyPointSettings: EmptyPointSettings(),
+                        width: 0.4,
+                        spacing: 0.2,
+                        borderRadius: const BorderRadius.only(
+                          topLeft: Radius.circular(8),
+                          topRight: Radius.circular(8),
+                        ),
+                        xValueMapper: (PrGraphAllContent data, _) => data.Month,
+                        yValueMapper: (PrGraphAllContent data, _) =>
+                            data.TotalAmount,
                       ),
-                      dataSource: data.data,
-                      width: 0.5,
-                      color: Colors.blue,
-                      xValueMapper: (PrGraphAllContent data, _) => data.Month,
-                      yValueMapper: (PrGraphAllContent data, _) =>
-                          data.TotalQty,
-                    ),
-                    ColumnSeries<PrGraphAllContent, String>(
-                      borderRadius: const BorderRadius.only(
-                        topLeft: Radius.circular(20),
-                        topRight: Radius.circular(20),
-                      ),
-                      dataSource: data.data,
-                      width: 0.5,
-                      color: Colors.green,
-                      xValueMapper: (PrGraphAllContent data, _) => data.Month,
-                      yValueMapper: (PrGraphAllContent data, _) =>
-                          data.TotalAmount,
-                    ),
                   ],
                 );
               },
               error: (error, stack) => Center(child: Text('Error: $error')),
               loading: () => const CircularProgressIndicator(),
             ),
+            // ElevatedButton(
+            //   onPressed: () {
+            //     setState(() {
+            //       showQty = !showQty;
+            //     });
+            //   },
+            //   child: Text(showQty ? 'Show Amount' : 'Show Qty'),
+            // ),
           ],
         ),
       ),
     );
   }
 }
-
-// class ChartColumnData {
-//   ChartColumnData({this.x, this.y, this.y1});
-//   final String? x;
-//   final double? y;
-//   final double? y1;
-// }
-
-// final List<ChartColumnData> chartData = <ChartColumnData>[
-//   ChartColumnData(x: 'Mo', y: -0.3, y1: 1.7),
-//   ChartColumnData(x: "Tu", y: -0.5, y1: 0.4),
-//   ChartColumnData(x: "We", y: -0.4, y1: 1),
-//   ChartColumnData(x: "Th", y: -0.45, y1: 0.7),
-//   ChartColumnData(x: "Fr", y: -0.9, y1: 0.8),
-//   ChartColumnData(x: "Sa", y: -0.6, y1: 0.9),
-//   ChartColumnData(x: "Su", y: -0.5, y1: 1),
-// ];
-
-final List<PrGraphAllModel> chartData = <PrGraphAllModel>[];
